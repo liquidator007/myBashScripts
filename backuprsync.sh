c90af02a511b9d4bff50a2be7e1af471d868f482
liquidator007 <liquidator007@protonmail.com>
2018-10-29 21:46:40 +0100

    new changes

---
+++ backuprsync.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+fechahora=`date +%F"_"%H"-"%M"-"%S`
+fecha=`date +%F`
+backupdir="$PWD/rsync/backup_$fecha"
+sufijo="-$fechahora.old"
+clear
+echo "############  BACKUPS INCREMENTALES CON RSYNC  ############"
+
+	if [[ $# -ne 2 ]]; then
+		echo "Se deben pasar 2 parámetros"
+		echo
+		echo "USO: $0 <origen> <destino>"
+		echo
+	else
+		rsync -abvzhP --no-whole-file --suffix=$sufijo $1 $2
+		rsync -abvvP --delete --backup-dir=$backupdir $1 $2
+	fi			
